b77b8c757637ba69ca62197eed9908f269c776cd
kingsley <kingsley2036@qq.com>
2021-11-20 17:31:29 +0800

    创建三个实体及其关联

---
+++ src/entity/Comment.ts
@@ -0,0 +1,26 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    ManyToOne,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn
+} from 'typeorm';
+import {User} from "./User";
+import {Post} from "./Post";
+@Entity('comments')
+export class Comment {
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+    @Column('text')
+    content: string;
+    @CreateDateColumn()
+    createdAt: Date;
+    @UpdateDateColumn()
+    updatedAt: Date;
+    @ManyToOne(type => User,user=>user.comments)
+    user:User;
+    @ManyToOne(type => Post, post => post.comments)
+    post: Post;
+
+}
---
+++ src/entity/User.ts
@@ -0,0 +1,21 @@
+import {Column, CreateDateColumn, Entity, PrimaryGeneratedColumn,OneToMany, UpdateDateColumn} from "typeorm";
+import {Post} from "./Post";
+import {Comment} from "./Comment";
+
+@Entity('users')
+export class User {
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+    @Column('varchar')
+    username: string;
+    @Column('varchar')
+    passwordDigest: string;
+    @CreateDateColumn()
+    createdAt: Date;
+    @UpdateDateColumn()
+    updatedAt: Date;
+    @OneToMany(type=>Post,post=>post.author)
+    posts:Post[];
+    @OneToMany(type=>Comment,comment=>comment.user)
+    comments:Comment[];
+}
