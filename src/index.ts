a13966842932f4c62478f5a9ad9c55953bc20ad4
kingsley <kingsley2036@qq.com>
2021-11-21 15:00:26 +0800

    connection bug 解决

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
-
+import {getDatabaseConnection} from '../lib/getDatabaseConnection';
 
 createConnection().then(async connection => {
+    const connect = await getDatabaseConnection()
+    console.log('connect',connect);
     console.log(connection);
     connection.close()
 
