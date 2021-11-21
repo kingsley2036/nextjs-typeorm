96f71bffb00f7d64267bd15d519bf5e8186d1da0
kingsley <kingsley2036@qq.com>
2021-11-21 17:12:57 +0800

    成功展示 posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,32 @@
 import {GetServerSideProps, NextPage} from 'next';
-import {UAParser} from 'ua-parser-js';
-import {useEffect, useState} from 'react';
+import React,from 'react';
+import {getDatabaseConnection} from "../lib/getDatabaseConnection";
+import {Post} from "../src/entity/Post";
 
 type Props = {
-  browser: {
-    name: string;
-    version: string;
-    major: string;
-  }
+      posts:Post[]
 }
 const index: NextPage<Props> = (props) => {
-  const {browser} = props;
-  const [width, setWidth] = useState(0);
-  useEffect(() => {
-    const w = document.documentElement.clientWidth;
-    setWidth(w);
-  }, []);
+  const {posts} = props;
   return (
     <div>
-      <h1>你的浏览器是 {browser.name}</h1>
-      <h2>你的浏览器窗口大小是 {width} 像素</h2>
+
+      {posts.map(post=>{
+        return <div>{post.id} {post.title}</div>
+      })}
     </div>
   );
 };
 export default index;
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const ua = context.req.headers['user-agent'];
-  const result = new UAParser(ua).getResult();
+export const getServerSideProps: GetServerSideProps = async () => {
+  const connection = await getDatabaseConnection();// 第一次链接能不能用 get
+  const posts = await connection.manager.find(Post);
+
+
   return {
     props: {
-      browser: result.browser
+      posts:JSON.parse(JSON.stringify(posts))
     }
   };
 };
