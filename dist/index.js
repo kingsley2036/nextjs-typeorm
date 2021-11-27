f0bb182408617a52791207efebe638b7f6917d67
kingsley <kingsley2036@qq.com>
2021-11-27 13:36:31 +0800

    成功创建user，即注册，不过没有检查username唯一性

--- dist/index.js
+++ dist/index.js
@@ -10,16 +10,25 @@ require("reflect-metadata");
 
 var _typeorm = require("typeorm");
 
+var _getDatabaseConnection = require("../lib/getDatabaseConnection");
+
 (0, _typeorm.createConnection)().then( /*#__PURE__*/function () {
   var _ref = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(connection) {
+    var connect;
     return _regenerator["default"].wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
+            _context.next = 2;
+            return (0, _getDatabaseConnection.getDatabaseConnection)();
+
+          case 2:
+            connect = _context.sent;
+            console.log('connect', connect);
             console.log(connection);
             connection.close();
 
-          case 2:
+          case 6:
           case "end":
             return _context.stop();
         }
